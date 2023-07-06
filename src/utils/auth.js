144b9fbc13cb0a09f41a9a7df53f803bad8021ae
zsf666 <3148436932@qq.com>
2023-07-06 16:42:59 +0800

    将Token存储的位置由Cookies变为localStorage，即本地存储

--- src/utils/auth.js
+++ src/utils/auth.js
@@ -1,15 +1,27 @@
-import Cookies from 'js-cookie'
+// import Cookies from 'js-cookie'
 
 const TokenKey = 'vue_admin_template_token'
 
+// export function getToken() {
+//   return Cookies.get(TokenKey)
+// }
+
+// export function setToken(token) {
+//   return Cookies.set(TokenKey, token)
+// }
+
+// export function removeToken() {
+//   return Cookies.remove(TokenKey)
+// }
+
 export function getToken() {
-  return Cookies.get(TokenKey)
+  return localStorage.getItem(TokenKey)
 }
 
 export function setToken(token) {
-  return Cookies.set(TokenKey, token)
+  return localStorage.setItem(TokenKey, token)
 }
 
 export function removeToken() {
-  return Cookies.remove(TokenKey)
+  return localStorage.removeItem(TokenKey)
 }
