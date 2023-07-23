d129f569da274e148d62438874febf48bacad6d2
zsf666 <3148436932@qq.com>
2023-07-23 20:39:00 +0800

    修改了首页样式

--- src/views/dashboard/index.vue
+++ src/views/dashboard/index.vue
@@ -5,30 +5,33 @@
 </template> -->
 <template>
   <el-card class="card">
+    <div class="info">小区信息</div>
     <div class="kuan">
-      <div :span="6" v-for="(item, index) in shuju" :key="index">
-        <div class="neirong">
-          <img :src="item.imgUrl" width="50" alt="..." />
-          <div class="right">
-            <div class="xia">{{ item.quantity }}</div>
-            <div class="shang">{{ item.title }}</div>
-          </div>
+      <div class="neirong" v-for="(item, index) in shuju" :key="index">
+        <img :src="item.imgUrl" width="40" alt="..." />
+        <div class="right">
+          <div class="xia">{{ item.quantity }}</div>
+          <div class="shang">{{ item.title }}</div>
+        </div>
+      </div>
+      <div class="nrxia">
+        <img :src="shuju[0].imgUrl" width="40" alt="..." />
+        <div class="right">
+          <div class="xia">{{ shuju[0].quantity }}</div>
+          <div class="shang">{{ shuju[0].title }}</div>
+        </div>
+      </div>
+      <div class="nrxia">
+        <img :src="shuju[0].imgUrl" width="40" alt="..." />
+        <div class="right">
+          <div class="xia">{{ shuju[0].quantity }}</div>
+          <div class="shang">{{ shuju[0].title }}</div>
         </div>
       </div>
     </div>
   </el-card>
 </template>
 
-<!-- <script>
-import { mapGetters } from "vuex";
-
-export default {
-  name: "Dashboard",
-  computed: {
-    ...mapGetters(["name"]),
-  },
-};
-</script> -->
 <script>
 export default {
   name: "",
@@ -37,25 +40,26 @@ export default {
       shuju: [
         {
           imgUrl: require("../../assets/用户.jpg"),
-          title: "用户数量",
+          title: "楼宇",
           quantity: "759",
         },
         {
           imgUrl: require("../../assets/电影票.jpg"),
-          title: "门票销售数量",
+          title: "房间",
           quantity: "26,845",
         },
         {
           imgUrl: require("../../assets/钱袋.jpg"),
-          title: "总销售额",
+          title: "车位",
           quantity: "1,234",
         },
         {
           imgUrl: require("../../assets/设备号管理.jpg"),
-          title: "设备数量",
+          title: "小区面积",
           quantity: "20",
         },
       ],
+      
     };
   },
 };
@@ -63,31 +67,53 @@ export default {
 
 <style lang="scss" scoped>
 .card {
-  margin: 30px;
+  margin: 10px;
+  width: 70%;
+  height: calc(400px - 60px);
+
+  .info {
+    font-weight: bold;
+    font-size: 20px;
+    margin: 0 0 15px 0;
+  }
 
   .kuan {
     display: flex;
-    width: 100%;
-    justify-content: space-around;
+    // justify-content: space-evenly;
+    flex-wrap: wrap; //让盒子向下换行
+
+    .neirong,.nrxia{
+      margin: 40px 0;
+    }
 
     .neirong {
-      margin: 25px;
       display: flex;
+      justify-content: space-evenly;
       align-items: center;
-      
+      width: 25%; //设置宽度，使盒子换行
+      border-bottom: 1px #ccc solid;
+
       .right {
-        margin: 5px;
         font-size: 16px;
 
-        .shang, .xia {
+        .shang,
+        .xia {
           text-align: center;
           margin: 10px 0;
         }
+
         .xia {
           font-weight: bold;
           font-size: 20px;
         }
       }
+    }
+
+    .nrxia {
+      width: 50%;
+      margin-top: 20px;
+      display: flex;
+      justify-content: space-around;
     }
   }
 }
